9152013d54107f041c9a641c241ac4adaba42f27
Hamza Kubba <hamza.kubba@gmail.com>
2019-01-21 15:02:08 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+import particleEmitterWrapper from './stores/ParticleEmitterWrapper';
+
+const format = (str, searchFilter) => {
+  return str
+    .replace(new RegExp(' ', 'g'), '&nbsp;')
+    .replace(new RegExp('(' + searchFilter + ')', 'g'), searchFilter ? '<span class="b black bg-light-blue">$1</span>' : '$1')
+    .replace(new RegExp('\\n', 'g'), '\n<br />')
+    ;
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchFilter: '',
+      collapsedRules: ['normalize', 'box-sizing', 'images', 'code'],
+      // showMatchesMode: 'withContext', // 'onlyMatches', 'complete'
+    };
+  }
+  render() {
+    const { searchFilter, collapsedRules } = this.state;
+
+    const rules = searchFilter
+      ? particleEmitterWrapper.filteredRules(searchFilter)
+      : particleEmitterWrapper.rules;
+
+    const results = [];
+
+    rules.forEach(([ ruleGroup, ruleCss ]) => {
+      let previousClass;
+      let currentGroup;
+      const htmlGroups = [];
+      results.push([ruleGroup, htmlGroups]);
+      ruleCss.forEach(([theClass, theRule]) => {
+        if (previousClass !== theClass) {
+          if (currentGroup) {
+            htmlGroups.push([previousClass, currentGroup]);
+          }
+          currentGroup = [];
+          previousClass = theClass;
+        }
+        currentGroup.push(theRule);
+      });
+      htmlGroups.push([previousClass, currentGroup]);
+    });
+
+    return (
+      <div className="bg-white flex flex-column h-100">
+        <div className="pa2 bg-gray">
+          <div className="pl3 dib">
+            <div className="pa2 white">
+              Filter:
+            </div>
+          </div>
+          <div className="bg-white dib">
+            <input className="pa2 w5 outline-0 b--none bw0" type="text" defaultValue={ searchFilter } onChange={ e => this.setState({ searchFilter: e.target.value.replace(new RegExp('([^a-zA-Z0-9]+)', 'g'), '\\$1') }) } />
+          </div>
+          <div className="white dib pa2 pl4">
+            Responsive variations:
+          </div>
+          <div className="dib">
+            <div
+              className="pa2 pointer br2 bg-near-white hover-bg-washed-green"
+              onClick={() => particleEmitterWrapper.setIncludeMediaQueryRules(!particleEmitterWrapper.includeMediaQueryRules)}>
+              { particleEmitterWrapper.includeMediaQueryRules ? 'Hide' : 'Show' }
+            </div>
+          </div>
+          <div className="dib pa2 fr">
+            <a className="white no-underline" href="https://github.com/hamzakubba/particle-emitter-documentor/">Source</a>
+          </div>
+        </div>
+        <div className="overflow-auto">
+          <div className="pl4 pv3 f6">
+            CSS below is a close approximation of
+            { ' ' }
+            <a className="green no-underline" href="http://tachyons.io/">Tachyons v4.10.0</a>
+            { ' ' }
+            CSS, generated using
+            { ' ' }
+            <a className="green no-underline" href="https://github.com/hamzakubba/particle-emitter/">particle-emitter</a>.
+            { ' ' }
+            This app's source is available:
+            { ' ' }
+            <a className="green no-underline" href="https://github.com/hamzakubba/particle-emitter-viewer/">particle-emitter-viewer</a>.
+          </div>
+          {
+            results.map(([ ruleName, htmlGroups ]) => {
+
+              const isCollapsed = (collapsedRules.indexOf(ruleName) > -1);
+              const expandGroup = () => {
+                collapsedRules.splice(collapsedRules.indexOf(ruleName), 1);
+                this.forceUpdate();
+              };
+              const collapseGroup = () => {
+                collapsedRules.push(ruleName);
+                this.forceUpdate();
+              };
+              const onClickGroup = isCollapsed ? expandGroup : collapseGroup;
+
+              let ruleCount = 0;
+              htmlGroups.forEach(([ , theGroup ]) =>
+                theGroup.forEach(theRule => theRule && theRule !== particleEmitterWrapper.responsiveAvailableText && ruleCount++));
+
+              const formatRules = ruleGroup => format(ruleGroup.join('\n'), searchFilter);
+
+              return (
+                <div key={ ruleName }>
+                  <div className="pl4 b f2 pv4 b--black bt pointer" onClick={ () => onClickGroup() }>
+                    { ruleName } ({ ruleCount })
+                    <div className="dib w2 h2 ml3 relative">
+                      <div style={{ transition: 'transform .2s' }} className={ 'absolute top-0 left-0 ' + (isCollapsed ? '' : 'rotate-90 pl1') }>⮞</div>
+                    </div>
+                  </div>
+                  {
+                    !isCollapsed
+                    && (
+                      <div>
+                        {
+                          htmlGroups.map(([ theClass, ruleGroup ], hgIndex) => (
+
+                            theClass
+                              ? (
+                                <div className="flex mb2 hover-bg-washed-green" key={ ruleName+hgIndex }>
+                                  <div className={ 'pa1 pl6 w-20' }>
+                                    <code className="pa1" dangerouslySetInnerHTML={{ __html: format((theClass || ''), searchFilter) }} />
+                                  </div>
+                                  <div className={ 'pa1 w-80' }>
+                                    { theClass && '{ ' }
+                                    <code className="mb1 pa1" dangerouslySetInnerHTML={{ __html: formatRules(ruleGroup) }} />
+                                    { theClass && ' }' }
+                                  </div>
+                                </div>
+                              ) : (
+                                <div className="flex mb2" key={ ruleName+hgIndex }>
+                                  <div className={ 'pa1 pl6 w-100' }>
+                                    <code className="mb1 pa1" dangerouslySetInnerHTML={{ __html: formatRules(ruleGroup) }} />
+                                  </div>
+                                </div>
+                              )
+
+                          ))
+                        }
+                      </div>
+                    )
+                  }
+                </div>
+              );
+
+            })
+          }
+          {
+            !results.length
+            && (
+              <div className="pl4 pv4 f3">
+                No matches found.
+              </div>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default observer(App);
---
+++ src/stores/ParticleEmitterWrapper.js
@@ -0,0 +1,112 @@
+import { types } from 'mobx-state-tree';
+
+import ParticleEmitter from 'particle-emitter';
+
+import tachyonsConfig from 'particle-emitter/build/tachyons-config';
+
+const cssStringToArray = str => {
+  const result = [];
+  str.split('\n').forEach(line => {
+    const matches = line.match(/^([^{]+){([^}]+)}/);
+    if (matches === null) {
+      result.push(['', line]);
+    } else {
+      let [, theClass, theRule] = matches;
+      theClass = (theClass || '').trim();
+      theRule = (theRule || '').trim();
+      result.push([theClass, theRule])
+    }
+  });
+  return result;
+};
+
+const ParticleEmitterWrapperModel = types.model('ParticleEmitterWrapper', {
+  particleEmitterInstance: types.frozen(),
+  includeMediaQueryRules: false,
+  responsiveAvailableText: '/*** Responsive variations available ***/\n\n',
+})
+  .views(self => ({
+    get json() {
+      return self.particleEmitterInstance.getJson()
+    },
+    get css() {
+      return self.particleEmitterInstance.getCss()
+    },
+  }))
+  .views(self => ({
+    get cssArray() {
+      return self.css.split('\n');
+    },
+    getRuleCss(ruleKey) {
+      const result = [];
+
+      const [, ruleValue] = self.json.rules.filter(([key]) => key === ruleKey)[0];
+
+      cssStringToArray(self.particleEmitterInstance.getRuleCss({
+        key: ruleKey,
+        value: ruleValue,
+      })).forEach(classRuleEntry => {
+        result.push(classRuleEntry);
+      });
+
+      if (ruleValue.repeatForMediaQueries) {
+        if (self.includeMediaQueryRules) {
+          self.json.mediaQueries.forEach(
+            ([mediaQuery]) => {
+              cssStringToArray(self.particleEmitterInstance.getRuleCss({
+                key: ruleKey,
+                value: ruleValue,
+                mediaQuery: mediaQuery,
+              })).forEach(classRuleEntry => {
+                result.push(classRuleEntry);
+              });
+            }
+          );
+        } else {
+          result.push(['', self.responsiveAvailableText]);
+        }
+      }
+
+      return result;
+    },
+  }))
+  .views(self => ({
+    get rules() {
+      const result = [];
+      self.json.rules.forEach(
+        ([ruleKey]) => result.push([ruleKey, self.getRuleCss(ruleKey)])
+      );
+      return result;
+    },
+  }))
+  .views(self => ({
+    filteredRules(filter) {
+      filter = new RegExp(filter);
+      return self.rules.filter(([ruleGroup, ruleCss]) => {
+        if (ruleGroup.match(filter) !== null) return true;
+        let matchFound = false;
+        ruleCss.forEach(([theClass, theRule]) => {
+          if (!matchFound && (theClass.match(filter) || theRule.match(filter))) {
+            matchFound = true;
+          }
+        });
+        return matchFound;
+      });
+    },
+  }))
+
+
+  .actions(self => ({
+    setNewInstance(instance) {
+      self.particleEmitterInstance = instance;
+    },
+    setIncludeMediaQueryRules(value) {
+      self.includeMediaQueryRules = !!value;
+    },
+  }))
+;
+
+
+export default ParticleEmitterWrapperModel.create({
+  particleEmitterInstance: new ParticleEmitter(tachyonsConfig),
+});
